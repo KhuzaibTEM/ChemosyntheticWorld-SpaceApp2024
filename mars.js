775257015784b318f18ea5df17361620717182f8
Khuzaib <79333922+KhuzaibTEM@users.noreply.github.com>
2024-11-15 04:20:14 +0500

    Update mars.js
    Size Adjustment made for Mars

--- mars.js
+++ mars.js
@@ -1,39 +1,33 @@
 import * as THREE from 'https://cdn.skypack.dev/three@0.129.0/build/three.module.js';
 import { GLTFLoader } from 'https://cdn.skypack.dev/three@0.129.0/examples/jsm/loaders/GLTFLoader.js';
-import { OrbitControls } from 'https://cdn.skypack.dev/three@0.129.0/examples/jsm/controls/OrbitControls';
 
-const camera = new THREE.PerspectiveCamera(
-    10,
-    window.innerWidth / window.innerHeight,
-    0.1,
-    1000
-);
-
-camera.position.z = 30;
-camera.position.y = - window.innerHeight / 700;
-camera.position.x = window.innerWidth / 380;
+// Camera setup
+const camera = new THREE.PerspectiveCamera(10, window.innerWidth / window.innerHeight, 0.1, 1000);
+camera.position.set(window.innerWidth / 380,  - window.innerHeight / 700, 30);
 
 const scene = new THREE.Scene();
 
 let planet;
 const loader = new GLTFLoader();
-loader.load('models/Mars.gltf',
+loader.load(
+    'models/Mars.gltf',
     function (gltf) {
         planet = gltf.scene;
         scene.add(planet);
     },
-    function (xhr) {},
-    function (error) {}
+    undefined,
+    function (error) {
+        console.error(error);
+    }
 );
 
-const renderer = new THREE.WebGLRenderer({alpha: true});
+// Renderer setup
+const renderer = new THREE.WebGLRenderer({ alpha: true });
+renderer.setPixelRatio(window.devicePixelRatio);
 renderer.setSize(window.innerWidth, window.innerHeight);
-renderer.domElement.style.width = '100%';
-renderer.domElement.style.height = '100%';
 document.getElementById('container3D-v2').appendChild(renderer.domElement);
 
-
-// Light
+// Lighting setup
 const ambientLight = new THREE.AmbientLight(0xffffff, 1.3);
 scene.add(ambientLight);
 
@@ -41,19 +35,21 @@ const topLight = new THREE.DirectionalLight(0xffffff, 1);
 topLight.position.set(500, 500, 500);
 scene.add(topLight);
 
+// Handle screen resizing
+window.addEventListener('resize', () => {
+    // Update the camera aspect ratio and projection matrix
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    
+    // Resize the renderer
+    renderer.setSize(window.innerWidth, window.innerHeight);
+});
 
+// Animation loop
 const reRender3D = () => {
     requestAnimationFrame(reRender3D);
-    scene.rotation.y += 0.005
+    scene.rotation.y += 0.005;
     renderer.render(scene, camera);
 };
 
 reRender3D();
-
-// window.addEventListener('resize', () => {
-//     const container = document.getElementById('container3D-v2');
-//     camera.aspect = container.clientWidth / container.clientHeight;
-//     camera.updateProjectionMatrix();
-//     renderer.setSize(container.clientWidth, container.clientHeight);
-// });
-
